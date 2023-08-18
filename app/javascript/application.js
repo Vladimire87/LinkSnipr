c33caee8291268b99579f2f11ad97e7e2f6e5c21
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-18 15:26:31 +0400

    add copy to clipboard to link

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,10 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
+import '@hotwired/turbo-rails'
+import './controllers'
+import * as bootstrap from 'bootstrap'
+
+import { Application } from '@hotwired/stimulus'
+import Clipboard from 'stimulus-clipboard'
+
+const application = Application.start()
+application.register('clipboard', Clipboard)
